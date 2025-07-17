729e3e8b35035e11e7b5dd3b4d6960146a114cf0
Siphelele@123 <siphelelegwabavu@gmail.com>
2025-07-17 08:57:42 +0200

    Update User.java

--- src/main/java/com/library/User.java
+++ src/main/java/com/library/User.java
@@ -1,11 +1,15 @@
 package com.library;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String username;
     private String password;
     private String name;
     private String contact;
     private boolean isAdmin;
+    private List<String> borrowedBooks = new ArrayList<>();
 
     public User(String username, String password, String name, String contact, boolean isAdmin) {
         this.username = username;
@@ -49,5 +53,15 @@ public class User {
 
     public void setAdmin(boolean isAdmin) {
         this.isAdmin = isAdmin;
+    }
+    
+    public boolean hasBorrowedBook(String bookId) {
+        return borrowedBooks.contains(bookId);
+    }
+
+    public void borrowBook(String bookId) {
+        if (!hasBorrowedBook(bookId)) {
+            borrowedBooks.add(bookId);
+        }
     }
 }
